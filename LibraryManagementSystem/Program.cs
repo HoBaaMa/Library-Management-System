09494b2d92e3a0a3d4c93ea5ee29ce65cb481346
ehab <ehab.sadiq89@gmail.com>
2024-08-13 02:28:57 +0300

    Creating The App Startup With Some Validation

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome To The Libaray System.");
+            Console.WriteLine("Are You Librarian or Regular User? (L/R)");
+            Console.ResetColor();
+            char userType = Console.ReadLine().ToUpper()[0];
+            if (userType == 'L')
+            {
+
+            }
+            else if (userType == 'R')
+            {
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please Enter Valid Input (L/R)!");
+                Console.ResetColor();
+            }
         }
     }
 }
